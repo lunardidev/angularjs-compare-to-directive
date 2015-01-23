4580d01967b4b95a0fe2dad39ab8e4b7ec1d787a
Felipe Farias <felipe@fetmobile.net>
2015-01-23 00:15:53 -0200

    angularjs-compareto

---
+++ compareto.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc       directive
+ * @name        ffariasdev.directive:compareto
+ * @description This is a directive to validate whether two fields are equal. 
+ */
+angular.module('ffariasdev')
+    .directive('compareTo', function() {
+        return {
+            scope: {
+                targetModel: '=compareTo'
+            },
+            require: 'ngModel',
+            link: function postLink(scope, element, attrs, ctrl) {
+
+                var compare = function() {
+
+                    var e1 = element.val();
+                    var e2 = scope.targetModel;
+
+                    if (e2 !== null) {
+                        return e1 === e2;
+                    }
+                    
+                    return false;
+                };
+
+                scope.$watch(compare, function(newValue) {
+                    ctrl.$setValidity('errorCompareTo', newValue);
+                });
+
+            }
+        };
+    });
